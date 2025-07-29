7667ce3539a2998b5b01ac6128384d4be48af885
pranav-d05 <pranavdhebe93@gmail.com>
2025-07-29 12:53:04 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = 'medical_chatbot',
+    version='0.1.0',
+    author = 'Pranav Dhebe',
+    author_email= 'pranavdhebe93@gmail.com',
+    packages= find_packages(),
+    install_requires=[]
+)
